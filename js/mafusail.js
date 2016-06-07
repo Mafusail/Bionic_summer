e46c610e6db921508567700487e60e5e85b862ee
Mafusail <shapovalenko.a.a@gmail.com>
2016-06-07 20:45:54 +0300

    block №1 (time, not ready)

--- js/mafusail.js
+++ js/mafusail.js
@@ -52,26 +52,6 @@ $(document).ready(function(){
 
     // счетчик
 
-    var clock;
-    clock = $(".clock").FlipClock({
-        clockFace : "DailyCounter",
-        autoStart : false,
-        callbacks : {
-            stop : function () {
-                $(".massage").html("Время прошло");
-            }
-        }
-    });
-
-    var dt = "July 04 2016 00:00:00";
-    var first = new Date(dt);
-    var last = Date.now();
-    var remaining = first - last;
-    remaining /=1000;
-
-    clock.setTime(remaining);
-    clock.setCountdown(true);
-    clock.start();
 
 
 
