70fb84252ad96e87a5db651b7a56fdbf8c2a2f39
Zachinskiy <R_G_C@i.ua>
2016-06-08 11:04:06 +0300

    Add files via upload

---
+++ js/jquery.paulund_modal_box.js
@@ -0,0 +1,114 @@
+/**
+ * JQuery Plugin for a modal box
+ * Will create a simple modal box with all HTML and styling
+ * 
+ * Author: Paul Underwood
+ * URL: http://www.paulund.co.uk
+ * 
+ * Available for free download from http://www.paulund.co.uk
+ */
+
+(function($){
+
+	// Defining our jQuery plugin
+
+	$.fn.paulund_modal_box = function(prop){
+
+		// Default parameters
+
+		var options = $.extend({
+			height : "350",
+			width : "600",
+			title:"Докладний список тем, які охоплює програма:",
+			description: "Вступ до web-технологій, Інструменти розробки верстальника, Основи верстки, Блокова верстка, Робота з зображеннями, Фотошоп для верстальника, Модульна верстка, Позиціонування, Форми та їх елементи, Адаптивна верстка, Флексбокс, Таблична верстка, Робота з фреймворками на прикладі Bootstrap / Materialize, Git, Workflow frontend-проекту, Вступ до JavaScript, Синтаксис та типи даних, Вирази та інструкції, Функції, Масиви та об’єкти, Browser Object Model (BOM), Збереження даних на клієнті, Document Object Model (DOM), Бібліотека jQuery, AJAX, JSON-сервер", 
+			top: "20%",
+			left: "30%",
+		},prop);
+				
+		//Click event on element
+		return this.click(function(e){
+			add_block_page();
+			add_popup_box();
+			add_styles();
+			
+			$('.paulund_modal_box').fadeIn();
+		});
+		
+		/**
+		 * Add styles to the html markup
+		 */
+		 function add_styles(){			
+			$('.paulund_modal_box').css({ 
+				'position':'absolute', 
+				'left':options.left,
+				'top':options.top,
+				'display':'none',
+				'height': options.height + 'px',
+				'width': options.width + 'px',
+				'border':'1px solid #fff',
+				'box-shadow': '0px 2px 7px #292929',
+				'-moz-box-shadow': '0px 2px 7px #292929',
+				'-webkit-box-shadow': '0px 2px 7px #292929',
+				'border-radius':'10px',
+				'-moz-border-radius':'10px',
+				'-webkit-border-radius':'10px',
+				'background': '#f2f2f2', 
+				'z-index':'50',
+			});
+			$('.paulund_modal_close').css({
+				'position':'relative',
+				'top':'-25px',
+				'left':'20px',
+				'float':'right',
+				'display':'block',
+				'height':'50px',
+				'width':'50px',
+				'background': 'url(img/close.png) no-repeat',
+			});
+			$('.paulund_block_page').css({
+				'position':'absolute',
+				'top':'0',
+				'left':'0',
+				'background-color':'rgba(0,0,0,0.6)',
+				'height':'100%',
+				'width':'100%',
+				'z-index':'10'
+			});
+			$('.paulund_inner_modal_box').css({
+				'background-color':'#fff',
+				'height':(options.height - 50) + 'px',
+				'width':(options.width - 50) + 'px',
+				'padding':'10px',
+				'margin':'15px',
+				'border-radius':'10px',
+				'-moz-border-radius':'10px',
+				'-webkit-border-radius':'10px'
+			});
+		}
+		
+		 /**
+		  * Create the block page div
+		  */
+		 function add_block_page(){
+			var block_page = $('<div class="paulund_block_page"></div>');
+						
+			$(block_page).appendTo('body');
+		}
+		 	
+		 /**
+		  * Creates the modal box
+		  */
+		 function add_popup_box(){
+			 var pop_up = $('<div class="paulund_modal_box"><a href="#" class="paulund_modal_close"></a><div class="paulund_inner_modal_box"><h2>' + options.title + '</h2><p>' + options.description + '</p></div></div>');
+			 $(pop_up).appendTo('.paulund_block_page');
+			 			 
+			 $('.paulund_modal_close').click(function(){
+				$(this).parent().fadeOut().remove();
+				$('.paulund_block_page').fadeOut().remove();				 
+			 });
+		}
+
+		return this;
+	};
+	
+})(jQuery);
