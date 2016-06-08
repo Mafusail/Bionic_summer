eff863620248c9e00320f10ead9e31ba4ae8bebe
Mafusail <shapovalenko.a.a@gmail.com>
2016-06-08 21:54:42 +0300

    time block

--- js/mafusail.js
+++ js/mafusail.js
@@ -91,7 +91,7 @@ function time() {
     tsec=today%60; today=Math.floor(today/60); if(tsec<10)tsec='0'+tsec;
     tmin=today%60; today=Math.floor(today/60); if(tmin<10)tmin='0'+tmin;
     thour=today%24; today=Math.floor(today/24);
-    timestr=today +"&#160;&#160;&#160;&#160;&#160;&#160;"+ thour+"&#160;&#160;&#160;&#160;&#160;"+tmin+"&#160;&#160;&#160;&#160;&#160;&#160;"+tsec+"";
+    timestr=today +"&#160;&#160;&#160;&#160;&#160;&#160;&#160;"+ thour+"&#160;&#160;&#160;&#160;&#160;&#160;&#160;"+tmin+"&#160;&#160;&#160;&#160;&#160;&#160;"+tsec+"";
     document.getElementById('t').innerHTML=timestr;
     window.setTimeout("time()",1000);
 }
